3665d81cee049a80e580e198b4b22ac25c19c30c
metelkin <metelkin@gnivc.ru>
2026-02-12 08:48:04 +0300

    Добавил новые картинки

--- src/components/GalleryComponent/assets/galleryItems.ts
+++ src/components/GalleryComponent/assets/galleryItems.ts
@@ -74,5 +74,144 @@ export const galleryItems = [
   {
     id: 15,
     src: pathImg(15),
+    columns: 2
+  },
+  {
+    id: 16,
+    src: pathImg(16),
+    rows: 2
+  },
+  {
+    id: 17,
+    src: pathImg(17),
+  },
+  {
+    id: 18,
+    src: pathImg(18),
+  },
+  {
+    id: 19,
+    src: pathImg(19),
+    columns: 2
+  },
+  {
+    id: 20,
+    src: pathImg(20),
+    columns: 2
+  },
+  {
+    id: 21,
+    src: pathImg(21),
+    rows: 2 
+  },
+  {
+    id: 22,
+    src: pathImg(22),
+  },
+  {
+    id: 23,
+    src: pathImg(23),
+  },
+  {
+    id: 24,
+    src: pathImg(24),
+    rows: 2 
+  },
+  {
+    id: 25,
+    src: pathImg(25),
+  },
+  {
+    id: 26,
+    src: pathImg(26),
+  },
+  {
+    id: 27,
+    src: pathImg(27),
+    columns: 2
+  },
+  {
+    id: 28,
+    src: pathImg(28),
+    columns: 2 
+  },
+  {
+    id: 29,
+    src: pathImg(29),
+    rows: 2
+  },
+  {
+    id: 30,
+    src: pathImg(30),
+  },
+  {
+    id: 31,
+    src: pathImg(31),
+  },
+  {
+    id: 32,
+    src: pathImg(32),
+    rows: 2 
+  },
+  {
+    id: 33,
+    src: pathImg(33),
+  },
+  {
+    id: 34,
+    src: pathImg(34),
+  },
+  {
+    id: 35,
+    src: pathImg(35),
+    columns: 2
+  },
+  {
+    id: 36,
+    src: pathImg(36),
+    rows: 2
+  },
+  {
+    id: 37,
+    src: pathImg(37),
+  },
+  {
+    id: 38,
+    src: pathImg(38),
+  },
+  {
+    id: 39,
+    src: pathImg(39),
+  },
+  {
+    id: 40,
+    src: pathImg(40),
+    rows: 2
+  },
+  {
+    id: 41,
+    src: pathImg(41),
+  },
+  {
+    id: 42,
+    src: pathImg(42),
+  },
+  {
+    id: 43,
+    src: pathImg(43),
+    columns: 2
+  },
+  {
+    id: 44,
+    src: pathImg(44),
+    rows: 2
+  },
+  {
+    id: 45,
+    src: pathImg(45),
+  },
+  {
+    id: 46,
+    src: pathImg(46),
   },
 ]
